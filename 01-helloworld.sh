141af7a5f035a916d44e5586736332e1676f7652
sivasoma <sivanaidusoma@gmail.com>
2024-01-14 23:49:52 +0530

    new version

--- 01-helloworld.sh
+++ 01-helloworld.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 echo "hi siva this is your new script"
+echo "hi sudheer this is your second script"
+echo "hi kalyan this is your new script"
+echo "hi sai this is your new script"
+echo "hi dileep this is your new script"
+echo "hi siva this is your second script"
+echo "hi siva this is your new script"
+echo "hi sudheer this is your new script"
+echo "hi siva this is your new script"
+
 
