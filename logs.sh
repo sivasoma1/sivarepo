6e08414fa2c3f623a2c0b30355db1bc11797970e
sivasoma <sivanaidusoma@gmail.com>
2024-01-17 12:40:22 +0530

    updating

---
+++ logs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+USERID=$(id -u)
+R=$\e[31m
+G=$\e[32m
+Y=$\e[33m
+N=$\e[0m
+Date=$(date)
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR: : $N please run as a root user"
+    exit 1
+
+fi
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo " installing..$2 $R is failure"
+        exit 1
+    else
+        echo " installing.. $2 $G is sucess"
+    fi
+}
+for i in $@
+
+do
+    yum install $i
+    if [ $? -ne 0 ]
+    then 
+        echo -e "$1 is not installed,$G now installing wait...."
+        yum install $i -y
+        validate $? "$i"
+    else
+        echo -e "$Y $i is already installed $N"
+    fi
+done
